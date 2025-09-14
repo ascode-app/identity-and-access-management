ee620339f080909cb22f13e5cdd5b145ea2e72e2
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2025-09-14 23:02:21 +0200

    Update okta-config.ts

--- hatch-project/src/hatch_project/apps/my-app/src/okta-config.ts
+++ hatch-project/src/hatch_project/apps/my-app/src/okta-config.ts
@@ -3,7 +3,9 @@ import { OktaAuth } from '@okta/okta-auth-js';
 // Okta Configuration - using hardcoded values for demo
 const OKTA_DOMAIN = 'integrator-1150200.okta.com';
 const CLIENT_ID = '0oaveu18q5ebi43io697';
-const REDIRECT_URI = 'http://localhost:4200/login/callback';
+// const REDIRECT_URI = 'http://localhost:4200/login/callback';
+// Below is specific for our ONA workspace
+const REDIRECT_URI = 'https://4200--019949ca-4f5c-7e6c-a3f0-2c843c088d1c.eu-central-1-01.gitpod.dev/login/callback';
 
 console.log('Okta Configuration:', {
   domain: OKTA_DOMAIN,
